5217e30252b1be5653600aca04c7ef6dd96764c8
shiona0820 <138412503+shiona0820@users.noreply.github.com>
2024-07-03 09:50:21 +0900

    a

--- GP24_ShionaMiyagi0630/Development/Objects/Time.Score/Fps.cpp
+++
@@ -1,9 +0,0 @@
-#include "Fps.h"
-
-#include "DxLib.h"
-
-//‰Šú‰»ˆ—
-void Fps::Initialize(float RefreshRate, int UpdateTime)
-{
-	fream_time = (int)(1000.0f / RefreshRate);   //ƒtƒŒ[ƒ€‚ÌŠÔ‚ÌŒvZ
-}
